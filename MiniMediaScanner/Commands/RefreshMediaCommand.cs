eafb737c71990abbf2da08523508f51a39c123e8
MusicMoveArr <jys78dfg@bottlepost.me>
2025-01-13 18:56:50 +0100

    Add Artist/Album filter to RefreshMediaCommand

--- MiniMediaScanner/Commands/RefreshMediaCommand.cs
+++ MiniMediaScanner/Commands/RefreshMediaCommand.cs
@@ -8,11 +8,15 @@ public class RefreshMetadataCommand
     /// Refresh metadata from files into the database
     /// </summary>
     /// <param name="connectionString">-C, ConnectionString for Postgres database.</param>
+    /// <param name="artist">-a, Artistname.</param>
+    /// <param name="album">-b, target Album.</param>
     [Command("refreshmetadata")]
-    public static void RefreshMetadata(string connectionString)
+    public static void RefreshMetadata(string connectionString,
+        string artist = "", 
+        string album = "")
     {
         var handler = new RefreshMetadataCommandHandler(connectionString);
 
-        handler.RefreshMetadata();
+        handler.RefreshMetadata(artist, album);
     }
 }
--- MiniMediaScanner/Commands/RefreshMediaCommandHandler.cs
+++ MiniMediaScanner/Commands/RefreshMediaCommandHandler.cs
@@ -7,34 +7,30 @@ public class RefreshMetadataCommandHandler
 {
     private readonly MetadataRepository _metadataRepository;
     private readonly ImportCommandHandler _importCommandHandler;
+    private readonly ArtistRepository _artistRepository;
 
     public RefreshMetadataCommandHandler(string connectionString)
     {
         _metadataRepository = new MetadataRepository(connectionString);
         _importCommandHandler = new ImportCommandHandler(connectionString);
+        _artistRepository = new ArtistRepository(connectionString);
+    }
+
+    public void RefreshMetadata(string album)
+    {
+        _artistRepository.GetAllArtistNames()
+            .ForEach(artist => RefreshMetadata(artist, album));
     }
     
-    public void RefreshMetadata()
+    public void RefreshMetadata(string artist, string album)
     {
-        int missingCount = 0;
-        const int limit = 1000;
-        int offset = 0;
-        while (true)
-        {
-            var metadata = _metadataRepository.GetAllMetadata(offset, limit);
-            offset += limit;
-
-            if (metadata.Count == 0)
-            {
-                break;
-            }
-
-            metadata
-                .AsParallel()
-                .WithDegreeOfParallelism(8)
-                .ForAll(m => _importCommandHandler.ProcessFile(m.Path, true));
-                
-        }
-        Console.WriteLine($"Total missing media files: {missingCount}");
+        var metadata = _metadataRepository.GetMetadataByArtist(artist)
+            .Where(metadata => string.IsNullOrWhiteSpace(album) || string.Equals(metadata.AlbumName, album, StringComparison.OrdinalIgnoreCase))
+            .ToList();
+        
+        metadata
+            .AsParallel()
+            .WithDegreeOfParallelism(8)
+            .ForAll(m => _importCommandHandler.ProcessFile(m.Path, true));
     }
 }
