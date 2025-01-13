170a908f288f2a65f62e93db814df6b8f44d6d6d
MusicMoveArr <jys78dfg@bottlepost.me>
2025-01-13 21:50:20 +0100

    Add Artist/Album filtering for DeletedMedia

--- MiniMediaScanner/Commands/DeletedMediaCommand.cs
+++ MiniMediaScanner/Commands/DeletedMediaCommand.cs
@@ -8,12 +8,25 @@ public class DeletedMediaCommand
     /// Check for deleted/missing music files on disk
     /// </summary>
     /// <param name="connectionString">-C, ConnectionString for Postgres database.</param>
+    /// <param name="artist">-a, Artistname.</param>
+    /// <param name="album">-b, target Album.</param>
     /// <param name="remove">-r, Remove records from database.</param>
     [Command("missing")]
-    public static void DeletedMedia(string connectionString, bool remove = true)
+    public static void DeletedMedia(string connectionString, 
+        bool remove = true,
+        string artist = "", 
+        string album = "")
     {
         var handler = new DeletedMediaCommandHandler(connectionString);
 
-        handler.CheckAllMissingTracks(remove);
+        if (string.IsNullOrWhiteSpace(artist))
+        {
+            handler.CheckAllMissingTracks(remove, album);
+        }
+        else
+        {
+            handler.CheckAllMissingTracks(remove, artist, album);
+        }
+        
     }
 }
--- MiniMediaScanner/Commands/DeletedMediaCommandHandler.cs
+++ MiniMediaScanner/Commands/DeletedMediaCommandHandler.cs
@@ -6,39 +6,46 @@ namespace MiniMediaScanner.Commands;
 public class DeletedMediaCommandHandler
 {
     private readonly MetadataRepository _metadataRepository;
+    private readonly ArtistRepository _artistRepository;
 
     public DeletedMediaCommandHandler(string connectionString)
     {
         _metadataRepository = new MetadataRepository(connectionString);
+        _artistRepository = new ArtistRepository(connectionString);
+    }
+
+    public void CheckAllMissingTracks(bool remove, string album)
+    {
+        _artistRepository.GetAllArtistNames()
+            .ForEach(artist => CheckAllMissingTracks(remove, artist, album));
     }
     
-    public void CheckAllMissingTracks(bool remove)
+    public int CheckAllMissingTracks(bool remove, string artist, string album)
     {
+        var metadata = _metadataRepository.GetMetadataByArtist(artist)
+            .Where(metadata => string.IsNullOrWhiteSpace(album) || string.Equals(metadata.AlbumName, album, StringComparison.OrdinalIgnoreCase))
+            .Where(metadata => File.Exists(metadata.Path))
+            .ToList();
+        
         int missingCount = 0;
-        const int limit = 1000;
-        int offset = 0;
-        while (true)
+
+        if (metadata.Count == 0)
         {
-            var metadata = _metadataRepository.GetAllMetadata(offset, limit);
-            offset += limit;
-
-            if (metadata.Count == 0)
-            {
-                break;
-            }
-            
-            var missing = metadata
-                .Where(metadata => !new FileInfo(metadata.Path).Exists)
-                .ToList();
-            
-            missingCount += missing.Count;
-            missing.ForEach(metadata => Console.WriteLine(metadata.Path));
-
-            if (remove && missing.Count > 0)
-            {
-                _metadataRepository.DeleteMetadataRecords(missing.Select(metadata => metadata.MetadataId).ToList());
-            }
+            return 0;
         }
-        Console.WriteLine($"Total missing media files: {missingCount}");
+        
+        var missing = metadata
+            .Where(metadata => !new FileInfo(metadata.Path).Exists)
+            .ToList();
+        
+        missingCount += missing.Count;
+        missing.ForEach(metadata => Console.WriteLine(metadata.Path));
+
+        if (remove && missing.Count > 0)
+        {
+            _metadataRepository.DeleteMetadataRecords(missing.Select(metadata => metadata.MetadataId).ToList());
+        }
+        Console.WriteLine($"Total missing media files: {missingCount} for artist '{artist}'");
+        return missingCount;
     }
 }
