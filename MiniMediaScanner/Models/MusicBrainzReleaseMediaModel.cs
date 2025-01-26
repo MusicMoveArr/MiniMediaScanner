0ff19c28a19aede1ab03a3d701e738f5e1ea37b9
MusicMoveArr <jys78dfg@bottlepost.me>
2025-01-26 14:02:51 +0100

    Fix JSON deserialization for track-count/offset

--- MiniMediaScanner/Models/MusicBrainzReleaseMediaModel.cs
+++ MiniMediaScanner/Models/MusicBrainzReleaseMediaModel.cs
@@ -1,17 +1,17 @@
-using Newtonsoft.Json;
+using System.Text.Json.Serialization;
 
 namespace MiniMediaScanner.Models;
 
 public class MusicBrainzReleaseMediaModel
 {
-    [JsonProperty(PropertyName = "track-count")]
+    [JsonPropertyName("track-count")]
     public int? TrackCount { get; set; }
     
     public string? Format { get; set; }
     public string? Title { get; set; }
     public int? Position { get; set; }
     
-    [JsonProperty(PropertyName = "track-offset")]
+    [JsonPropertyName("track-offset")]
     public int? TrackOffset { get; set; }
     
     public List<MusicBrainzReleaseMediaTrackModel>? Tracks { get; set; } = new List<MusicBrainzReleaseMediaTrackModel>();
