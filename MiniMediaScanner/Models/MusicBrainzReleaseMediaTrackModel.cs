01d4ebafbd6821f5419f1a883a56d0d5da5fb777
MusicMoveArr <jys78dfg@bottlepost.me>
2025-02-01 13:34:24 +0100

    Prevent errors on Musicbrainz Tracknumbers like A1/A2 etc

--- MiniMediaScanner/Models/MusicBrainzReleaseMediaTrackModel.cs
+++ MiniMediaScanner/Models/MusicBrainzReleaseMediaTrackModel.cs
@@ -1,3 +1,6 @@
+using System.Text.Json.Serialization;
+using MiniMediaScanner.JsonConverters;
+
 namespace MiniMediaScanner.Models;
 
 public class MusicBrainzReleaseMediaTrackModel
@@ -5,7 +8,10 @@ public class MusicBrainzReleaseMediaTrackModel
     public string? Id { get; set; }
     public string? Title { get; set; }
     public int? Length { get; set; }
+    
+    [JsonConverter(typeof(MusicBrainzReleaseMediaTrackModelJsonConverter))]
     public int? Number { get; set; }
+    
     public int? Position { get; set; }
     public MusicBrainzReleaseMediaTrackRecordingModel? Recording { get; set; }
 }
