c376e1dc9fd5bcad53a1ffb05fc3e1cbbcf263e2
MusicMoveArr <jys78dfg@bottlepost.me>
2025-07-01 20:41:31 +0200

    Update model for Import

--- MiniMediaScanner/Models/CanUpdateMetadataModel.cs
+++ MiniMediaScanner/Models/CanUpdateMetadataModel.cs
@@ -3,6 +3,7 @@ namespace MiniMediaScanner.Models;
 public class CanUpdateMetadataModel
 {
     public Guid MetadataId { get; set; }
+    public string Path { get; set; }
     public DateTime? File_LastWriteTime { get; set; }
     public DateTime? File_CreationTime { get; set; }
 }
