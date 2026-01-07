5d41a2ff3a2d89068e02e15840494d835a87890b
MusicMoveArr <jys78dfg@bottlepost.me>
2026-01-07 07:39:37 +0100

    Improved AcoustIdCheck command

--- MiniMediaScanner/Commands/AcoustIdCheckCommandHandler.cs
+++ MiniMediaScanner/Commands/AcoustIdCheckCommandHandler.cs
@@ -16,33 +16,25 @@ public class AcoustIdCheckCommandHandler
     public async Task SendSubmissionsAsync(string acoustidClientKey)
     {
         int limit = 500;
-        while (true)
+        var submissionIds = await _acoustIdSubmissionRepository.GetCheckMetadataIdsAsync();
+
+        Console.WriteLine($"Checking {submissionIds.Count} submissions");
+
+        for (int offset = 0; offset < submissionIds.Count; offset += limit)
         {
-            var submissionIds = await _acoustIdSubmissionRepository.GetCheckMetadataIdsAsync();
-
-            if (!submissionIds.Any())
-            {
-                break;
-            }
-
-            Console.WriteLine($"Checking {submissionIds.Count} submissions");
-
-            for (int offset = 0; offset < submissionIds.Count; offset += limit)
-            {
-                var ids = submissionIds
-                    .Skip(offset)
-                    .Take(limit)
-                    .ToList();
+            var ids = submissionIds
+                .Skip(offset)
+                .Take(limit)
+                .ToList();
+        
+            var response = await _acoustIdService.CheckStatusAsync(acoustidClientKey, ids);
             
-                var response = await _acoustIdService.CheckStatusAsync(acoustidClientKey, ids);
-                
-                foreach (var submission in response.Submissions.Where(s => s.Status != "pending"))
-                {
-                    await _acoustIdSubmissionRepository.UpdateSubmissionStatusAsync(
-                        submission.Id, 
-                        submission.Status,
-                        submission.Result.Id);
-                }
+            foreach (var submission in response.Submissions.Where(s => s.Status != "pending"))
+            {
+                await _acoustIdSubmissionRepository.UpdateSubmissionStatusAsync(
+                    submission.Id, 
+                    submission.Status,
+                    submission.Result.Id);
             }
         }
     }
